9b3acfaf234bb5f2b603298ff51cbb4255bcce02
afjal4 <afjal.nc@gmail.com>
2025-12-09 17:03:43 +0000

    Some Kernel functions implemented

--- specific_connections_in_embeddings/connections_kernel/kernel_matrix.py
+++ specific_connections_in_embeddings/connections_kernel/kernel_matrix.py
@@ -1,16 +1,21 @@
-from sklearn.cluster import SpectralClustering
-from sklearn.metrics.pairwise import cosine_similarity
-def kernel_func(embeddings):
-    #this method will define the kernel function
+from scipy.cluster import SpectralClustering
+from scipy.metrics.pairwise import cosine_similarity
+from sentence_transformers import SentenceTransformer
+import numpy as np
+from scipy.spatial.distance import cdist
+
+
+def kernel_func(embeddings, ker_type):
+    # this method will do the kernel function
+    
+
 
     X = embeddings  # shape (16, d)
-    similarity = cosine_similarity(X)
-
-    clustering = SpectralClustering(
-        n_clusters=4,
-        affinity='precomputed',
-        random_state=0
-    ).fit(similarity)
-
-    groups = clustering.labels_
-    return groups
+    if ker_type == "cosine":
+        similarity = cosine_similarity(X)
+    elif ker_type == "RBF":
+        sq_dists = cdist(X, X, 'sqeuclidean')
+        dists = pdist(X, 'euclidean')
+        sigma = np.median(dists)
+        gamma = 1 / (2 * sigma**2)
+        similarity = np.exp(-gamma * sq_dists)
